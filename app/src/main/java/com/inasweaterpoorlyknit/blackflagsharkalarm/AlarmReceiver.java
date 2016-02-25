6ed8dcaeaf9114835b78f40c558c05702807fd7f
Lucodivo <chaskins483@student.msjc.edu>
2016-02-25 07:25:49 -0800

    commented AlarmReceiver.java

--- app/src/main/java/com/inasweaterpoorlyknit/blackflagsharkalarm/AlarmReceiver.java
+++ app/src/main/java/com/inasweaterpoorlyknit/blackflagsharkalarm/AlarmReceiver.java
@@ -4,12 +4,16 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+// a BroadcastRecever class that activates Alarm.java when called
 public class AlarmReceiver extends BroadcastReceiver {
+    // function called when 
     @Override
     public void onReceive(Context context, Intent intent) {
-        //Toast.makeText(context, "yoyomah", Toast.LENGTH_SHORT).show();
+        // intent that calls alarm class
         Intent alarmIntent = new Intent(context, Alarm.class);
+        // ensure the inent opens up a task for the activity being called(won't open another task if one is alreayd open for activity)
         alarmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        // start the activity
         context.startActivity(alarmIntent);
     }
 }
