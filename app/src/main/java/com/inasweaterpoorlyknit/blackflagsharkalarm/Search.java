2b30b1c462423379e9335fc04cb8bceff781b5d8
Lucodivo <chaskins483@student.msjc.edu>
2016-02-25 15:47:19 -0800

    Commented Search.java

--- app/src/main/java/com/inasweaterpoorlyknit/blackflagsharkalarm/Search.java
+++ app/src/main/java/com/inasweaterpoorlyknit/blackflagsharkalarm/Search.java
@@ -12,13 +12,13 @@ import com.google.api.services.youtube.model.SearchResult;
 import java.io.IOException;
 import java.util.List;
 
+// class used to return search results from YouTube as a List<SearchResult>
 public class Search {
     // number of videos we want the search function to return
     public static final long NUMBER_OF_VIDEOS_TO_RETURN = 10;
 
     // global instance of a YouTube object
     private static YouTube youtube;
-
 
     // list of SearchResult objects to hold the search results, takes a query and web browser key as arguments
     public static List<SearchResult> Search(String query, String developerKey){
@@ -34,20 +34,31 @@ public class Search {
             // Define the API request for the search results
             YouTube.Search.List search = youtube.search().list("id,snippet");
 
+            // user the YouTube browser developer key passes as the
             search.setKey(developerKey);
 
+            // set the query as the query passed
             search.setQ(query);
+            // only search for videos(not playlists or channels)
             search.setType("video");
+            // different fields to be returned from the JSON organized results
+            // id/kind (should always be "youtube#video")
+            // id/videoId = id of video (used to play)
+            // snippet/title = title of video (used for our playlist)
+            // snippet/thumbnails/default/url = urls of the thumbnails (used for listing results)
             search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+            // we only want NUMBER_OF_VIDEOS_TO_RETURN amount of videos
             search.setMaxResults(NUMBER_OF_VIDEOS_TO_RETURN);
 
-            // call api
+            // call YouTube API to get a search response
             SearchListResponse searchResponse = search.execute();
+            // extract the search results from the search response
             List<SearchResult> searchResultList = searchResponse.getItems();
+            // only return search results if anything was found
             if(searchResultList != null){
                 return searchResultList;
             }
-
+        // handle exceptions
         } catch (GoogleJsonResponseException e) {
             System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
                     + e.getDetails().getMessage());
@@ -57,6 +68,7 @@ public class Search {
             t.printStackTrace();
         }
 
+        // return null if search was unsuccessful
         return null;
     }
 }
